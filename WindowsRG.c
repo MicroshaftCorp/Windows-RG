37897d1996878173dc56c54c467e7a60b72e1033
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-08-11 11:17:29 -0700

    Create WindowsRG.c

---
+++ WindowsRG.c
@@ -0,0 +1,101 @@
+/*
+    * TOP SECRET Microsoft® Code
+    * Project: ItsReallyChicagoEvenThoughWeSayOtherwise™
+    * Projected release-date: Summer 1998
+    */
+    
+    #include "win31.h"
+    #include "win95.h"
+    #include "mac os 9.h"
+    #include "evenmore.h"
+    #include "oldstuff.h"
+    #include "viruses.h"
+    #include "8762234555_Funny_ERRORmsg"
+    #define INSTALL VERYHARD
+    
+    char make_prog_look_big[16000000];
+    
+    void main()
+    {
+        if (detect_cache())
+            disable_cache();
+                 
+        if (fast_cpu())
+        {
+            set_wait_states(lots);
+            mouse.setSpeed(very_fast);
+            mouse.setAction(jumpy);
+            mouse.setReaction(RANDOM);
+            mouse.setFuckUps(RANDOM);
+        }
+        
+        while(!CRASHED)
+        {
+            display_copyright_message();
+            display_BSOD_loop();
+            do_nothing_loop();
+            if (first_time_installation)
+            {
+                make_1_megabyte_swapfile();
+                do_nothing_loop();
+                totally_screw_up_EXT2_file_system();
+                Screw_and_destroy_the_rest_of_OS2();
+                kill_loads_of_flags_and_fuck_up_windows(NULL);
+                hang_system();
+            }
+            write_something(bill_gates_msg_to_your_mother);
+            display_copyright_message();
+            do_nothing_loop();
+            do_some_stuff();
+            if (still_not_crashed)
+            {
+                display_copyright_message();
+                do_nothing_loop();
+                basically_run_windows_3.1();
+                do_nothing_loop();
+                do_nothing_loop();
+            }
+        }
+       
+       /* printf("Welcome to Windows 3.11"); */
+       /* printf("Welcome to Windows 95"); */
+       printf("Welcome to Windows RG");
+       /* printf("Welcome to Windows 2000"); */
+       /* printf("Welcome to Windows XP"); */
+       /* printf("Welcome to Windows Vista"); */
+       /* printf("Welcome to Windows 7"); */
+       
+       if (system_ok())
+           crash(to_dos_prompt);
+       else
+           system_memory = open("a:\swp0001.swp", O_CREATE);
+       
+       while(something)
+       {
+           printf("Insert Windows 98 install CD into CD-ROM drive!")
+           sleep(5);
+           get_user_input();
+           sleep(5);
+           act_on_user_input();
+           sleep(5);
+           annoy_user();
+       }
+       
+       if (still_not_annoyed)
+       {
+           create_system_backup();
+           hang_system();
+       }
+       if (create_system_backup)
+       {
+           create_system_backup();
+           randomize_backup_file();
+       }
+       
+       create_general_protection_fault();
+       
+       while (keyboard_status == notpresent)
+       {
+            printf("Keyboard found. Press F1 to continue.");
+       }
+   }
