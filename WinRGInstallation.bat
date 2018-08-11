068492a41b3bdb74c1bafad35bca427fb1ba14eb
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-08-11 11:24:55 -0700

    Create WinRGInstallation.bat

---
+++ WinRGInstallation.bat
@@ -0,0 +1,11 @@
+@echo off
+title Windows RG Installer 2.9.0
+echo Welcome to Windows RG Installer 2.9.0!
+pause
+echo Installing Windows RG...
+timeout 10
+echo Pause!
+pause
+start Boot.c
+timeout 1
+exit
